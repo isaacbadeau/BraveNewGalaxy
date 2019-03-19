e6e5173096af18e978a3380903009079df55c325
Samuel Lane <samuel.lane.sa@outlook.com>
2019-03-19 14:22:28 -0400

    Player Select Option

---
+++ AWholeNewWorld/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    class Player
+    {
+        public string Name;
+        public int Age;
+        public string Planet;
+        public string Bio;
+
+        public Player(string Name, int Age, string Planet, string Bio)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Planet = Planet;
+            this.Bio = Bio;
+        }
+
+        public string GetInfo()
+        {
+            string output = Name + " " + Age + " " + Planet + " \n" + Bio;
+            return output;
+
+
+        }
+    }
+
+
+}
